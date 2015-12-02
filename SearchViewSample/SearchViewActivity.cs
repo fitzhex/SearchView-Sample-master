d96f2e46a43648a0ead88ae88643f328512dcea3
fitzhex <fitz.hex@gmail.com>
2015-12-02 18:10:48 +0800

    initial commit

---
+++ SearchViewSample/SearchViewActivity.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.Runtime;
+using Android.Support.V4.View;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Newtonsoft.Json;
+using System;
+
+namespace SearchViewSample
+{
+    [Activity(Label = "SearchView Sample", MainLauncher = true, Icon = "@drawable/icon",
+        Theme = "@style/Theme.AppCompat.Light")]
+    public class SearchViewActivity : ActionBarActivity
+    {
+        private SearchView _searchView;
+        private ListView _listView;
+        private ChemicalsAdapter _adapter;
+
+		List<Chemical> chemicals = new List<Chemical>();
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            SupportActionBar.SetDisplayShowHomeEnabled(true);
+
+             chemicals = new List<Chemical>
+            {
+                new Chemical {Name = "Niacin", DrawableId = Resource.Drawable.Icon},
+                new Chemical {Name = "Biotin", DrawableId = Resource.Drawable.Icon},
+                new Chemical {Name = "Chromichlorid", DrawableId = Resource.Drawable.Icon},
+                new Chemical {Name = "Natriumselenit", DrawableId = Resource.Drawable.Icon},
+                new Chemical {Name = "Manganosulfate", DrawableId = Resource.Drawable.Icon},
+                new Chemical {Name = "Natriummolybdate", DrawableId = Resource.Drawable.Icon},
+                new Chemical {Name = "Ergocalciferol", DrawableId = Resource.Drawable.Icon},
+                new Chemical {Name = "Cyanocobalamin", DrawableId = Resource.Drawable.Icon},
+            };
+
+            _listView = FindViewById<ListView>(Resource.Id.listView);
+            _adapter = new ChemicalsAdapter(this, chemicals);
+            _listView.Adapter = _adapter;
+
+
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.main, menu);
+
+            var item = menu.FindItem(Resource.Id.action_search);
+
+            var searchView = MenuItemCompat.GetActionView(item);
+            _searchView = searchView.JavaCast<SearchView>();
+
+           // _searchView.QueryTextChange += (s, e) => _adapter.Filter.InvokeFilter(e.NewText);
+
+            _searchView.QueryTextSubmit += (s, e) =>
+            {
+                // Handle enter/search button on keyboard here
+                Toast.MakeText(this, "Searched for: " + e.Query, ToastLength.Short).Show();
+//				_adapter.Filter.InvokeFilter(e.Query);
+			
+				try
+				{
+					var json = WebServices.getSearchResult(e.Query);
+					var ProdData = JsonConvert.DeserializeObject<WebServices.RootObject> (json);
+
+					Console.Error.WriteLine(ProdData.data[0].title);
+				}
+				catch
+				{
+					RunOnUiThread (() => {
+						chemicals.Clear();
+						_adapter.NotifyDataSetChanged ();
+						_adapter.NotifyDataSetInvalidated();
+
+					});
+
+					Console.Error.WriteLine("Tiada padanan"+chemicals.Count);
+				}
+				e.Handled = true;
+				_searchView.ClearFocus();
+            };
+
+            MenuItemCompat.SetOnActionExpandListener(item, new SearchViewExpandListener(_adapter));
+
+            return true;
+        }
+
+        private class SearchViewExpandListener 
+            : Java.Lang.Object, MenuItemCompat.IOnActionExpandListener
+        {
+            private readonly IFilterable _adapter;
+
+            public SearchViewExpandListener(IFilterable adapter)
+            {
+                _adapter = adapter;
+            }
+
+            public bool OnMenuItemActionCollapse(IMenuItem item)
+            {
+                _adapter.Filter.InvokeFilter("");
+                return true;
+            }
+
+            public bool OnMenuItemActionExpand(IMenuItem item)
+            {
+                return true;
+            }
+        }
+    }
+}
+
---
+++ SearchViewSample/Webservice.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace SearchViewSample
+{
+	public class WebServices
+	{
+		public static string getSearchResult(string keyword)
+		{
+
+			string requestStr = string.Format ("http://myshop.pi1m.my/api/product/search/"+keyword);
+			HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(requestStr);
+			Console.WriteLine ("[WebServices] SetMySkoolPetiMasuk string: {0}",requestStr);
+			httpWebRequest.Method = "GET";
+			httpWebRequest.ContentType = "application/x-www-form-urlencoded";
+			//httpWebRequest.ContentLength = data.Length;
+			//Stream myStream = httpWebRequest.GetRequestStream();
+			//myStream.Write(data,0,data.Length);
+			//myStream.Close();
+
+			string Json_Value1 = "";
+
+			try
+			{
+				using (var response = httpWebRequest.GetResponse ()) {
+
+					using (var reader = new StreamReader (response.GetResponseStream ())) {
+
+						Json_Value1 = reader.ReadToEnd ();
+						Console.WriteLine ("[WebServices] Response Init: "+Json_Value1);
+
+					}
+				}
+
+			}
+			catch(System.Exception e) {
+
+				Console.WriteLine ("[WebServices] Response Init: {0}",e);
+			}
+
+			return Json_Value1;
+		}
+
+		public class Photo
+		{
+			public string id { get; set; }
+			public string product_id { get; set; }
+			public string name { get; set; }
+			public string updated_at { get; set; }
+			public string created_at { get; set; }
+			public string deleted_at { get; set; }
+		}
+
+		public class Datum
+		{
+			public string id { get; set; }
+			public string sub_category_id { get; set; }
+			public string status { get; set; }
+			public string user_id { get; set; }
+			public string title { get; set; }
+			public string description { get; set; }
+			public string rating_cache { get; set; }
+			public string rating_count { get; set; }
+			public string viewer_count { get; set; }
+			public string viewer_count_by_week { get; set; }
+			public string search_count_by_week { get; set; }
+			public string week_viewer { get; set; }
+			public string week_search { get; set; }
+			public string comment_count { get; set; }
+			public string price { get; set; }
+			public object sku { get; set; }
+			public string date { get; set; }
+			public string term { get; set; }
+			public string updated_at { get; set; }
+			public string created_at { get; set; }
+			public string deleted_at { get; set; }
+			public string main_photo { get; set; }
+			public string url_photo_thumb { get; set; }
+			public string url_photo_large { get; set; }
+			public List<Photo> photos { get; set; }
+		}
+
+		public class RootObject
+		{
+			public int total { get; set; }
+			public int per_page { get; set; }
+			public int current_page { get; set; }
+			public int last_page { get; set; }
+			public int from { get; set; }
+			public int to { get; set; }
+			public List<Datum> data { get; set; }
+		}
+
+
+	}
+
+}
